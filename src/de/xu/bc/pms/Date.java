84e5b5db75f21399cb31545afd0b1a04bc2b45fc
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2022-03-28 15:05:57 +0200

    working with dates made easier

---
+++ src/de/xu/bc/pms/Date.java
@@ -0,0 +1,14 @@
+package de.xu.bc.pms;
+
+public class Date {
+    
+    final int day;
+    final int month;
+    final int year;
+
+    Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+}    
