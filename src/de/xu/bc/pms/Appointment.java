d395bdad81a9035a284d07bc38b927e41a06be0b
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2022-03-29 16:14:16 +0200

    fixed Stuff

--- src/de/xu/bc/pms/Appointment.java
+++ src/de/xu/bc/pms/Appointment.java
@@ -4,21 +4,21 @@ import java.time.LocalDateTime;
 
 public class Appointment
 {
-	private int appointmentID;
+	final private int appointmentID;
 
 	private static int sequence = 0;
 	
 	public Patient patient;
 
-	private LocalDateTime time;
+	final private LocalDateTime time;
 
-	private String treatment;
+	private String Symptoms;
 	
 	
-	public Appointment(Patient patient, String treatment) 
+	public Appointment(Patient patient, String Symptoms)
 	{
 		this.patient = patient;
-		this.treatment = treatment;
+		this.Symptoms = Symptoms;
 		this.appointmentID=++sequence;
 		this.time = LocalDateTime.now();
 		
@@ -27,7 +27,7 @@ public class Appointment
 	@Override
 	public String toString() 
 	{
-		return String.format("name: %s %s\ntreatment: %s\nappointmentID: %s\ntime: %s",patient.getName(), patient.getLastname(), treatment, appointmentID, time.toString());
+		return String.format("name: %s %s\ntreatment: %s\nappointmentID: %s\ntime: %s",patient.getName(), patient.getLastname(), Symptoms, appointmentID, time.toString());
 	
 	}
 }
--- src/de/xu/bc/pms/Storage.java
+++ src/de/xu/bc/pms/Storage.java
@@ -1,6 +1,5 @@
 package de.xu.bc.pms;
 
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -8,7 +7,7 @@ import java.util.ArrayList;
 public class Storage {
 
     private static List<Patient> patients = new ArrayList<>();
-    private static List<Admissions> admissions = new ArrayList<>();
+    private static List<Visit> admissions = new ArrayList<>();
     private static List<Appointment> appointments = new ArrayList<>();
 
     /**
@@ -34,21 +33,21 @@ public class Storage {
     /**
      * managing Admissions *
      */
-    public static void admitPatient(@NotNull Patient patient) {
-        int temp = (int) Math.random();
-        Admissions admit = new Admissions(Integer.toString(temp),Integer.toString(patient.getHealthID()));
+    public static void admitPatient(Patient patient, String reason) {
+        Visit admit = new Visit(Integer.toString(patient.getHealthID()), reason);
         admissions.add(admit);
     }
-    /**
-     * discharging patients would mean deleting them from the list, but there
-     * is a protocol that needs to be saved that has not jet have been implemented
-    **/
+    public static void dischargePatien(Patient patient, String diagnose) {
+        int i = admissions.indexOf(patient);
+        admissions.get(i).DischargePatien(diagnose);
+    }
 
     /**
      * Managing Appointmets
      */
-    public static void createAnAppontentForPatient(Patient patient) {
-
+    public static void createAnAppontentForPatient(Patient patient, String reason) {
+        Appointment appointment = new Appointment(patient, reason);
+        appointments.add(appointment);
     }
 
 }
